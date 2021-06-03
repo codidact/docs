a1ef15fda68385ddfd88597418b0233c080d1a77
sau226 <32805612+sau226@users.noreply.github.com>
2021-06-03 22:01:15 +0800

    Add Netlify badge

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,13 @@ module.exports = {
               label: 'Twitter',
               href: 'https://twitter.com/codidact',
             },
+            {
+              html: `
+                <a href="https://www.netlify.com" target="_blank" rel="noreferrer noopener" aria-label="Deploys by Netlify">
+                  <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
+                </a>
+              `,
+            },            
           ],
         },
         {
